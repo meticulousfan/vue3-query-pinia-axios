e4538df5dbedcf76a13299a84ec6f97ee3edda53
meticulousfan <elithofan@gmail.com>
2023-04-05 11:03:34 +0900

    Added VueQueryPlugin, toastify

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { VueQueryPlugin } from 'vue-query'
 import './index.css'
+import 'mosha-vue-toastify/dist/style.css'
 
 import App from './App.vue'
 import router from './router'
@@ -11,5 +13,6 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(VueQueryPlugin)
 
 app.mount('#app')
