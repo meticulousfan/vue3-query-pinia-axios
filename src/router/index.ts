81435ee62c82571f32a606ce872d521de9b19e92
meticulousfan <elithofan@gmail.com>
2023-04-09 10:26:22 +0900

    Added router index file

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,85 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import {
+  createRouter,
+  createWebHistory,
+  type NavigationGuardNext,
+  type RouteLocationNormalized,
+} from "vue-router";
+import HomeView from "@/views/HomeView.vue";
+import RegisterView from "@/views/RegisterView.vue";
+import LoginView from "@/views/LoginView.vue";
+import VerifyEmailView from "@/views/VerifyEmailView.vue";
+import { useAuthStore } from "@/stores/authStore";
+import requireAuth from "@/router/middleware/requireAuth";
+import middlewarePipeline from "@/router/middlewarePipeline";
+import ProfileViewVue from "@/views/ProfileView.vue";
+const routes = [
+  {
+    name: "home",
+    path: "/",
+    component: HomeView,
+  },
+  {
+    name: "register",
+    path: "/register",
+    component: RegisterView,
+  },
+  {
+    name: "login",
+    path: "/login",
+    component: LoginView,
+  },
+  {
+    name: "verifyemail",
+    path: "/verifyemail",
+    component: VerifyEmailView,
+    children: [
+      {
+        name: "verifyemail",
+        path: ":verificationCode",
+        component: VerifyEmailView,
+      },
+    ],
+  },
+  {
+    name: "profile",
+    path: "/profile",
+    component: ProfileViewVue,
+    meta: {
+      middleware: [requireAuth],
+    },
+  },
+];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+  history: createWebHistory('/'),
+  routes,
+});
 
-export default router
+router.beforeEach(
+  (
+    to: RouteLocationNormalized,
+    form: RouteLocationNormalized,
+    next: NavigationGuardNext
+  ) => {
+    const authStore = useAuthStore();
+
+    if (!to.meta.middleware) {
+      return next();
+    }
+
+    const middleware = to.meta.middleware as any;
+
+    const context = {
+      to,
+      from,
+      next,
+      authStore,
+    };
+
+    return middleware[0]({
+      ...context,
+      next: middlewarePipeline(context, middleware, 1),
+    });
+  }
+);
+export default router;
